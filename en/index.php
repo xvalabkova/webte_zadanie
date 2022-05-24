0f763db0da9128080288982791fc4e11277dd882
xvalabkova <xvalabkova@stuba.sk>
2022-05-24 15:49:51 +0200

    feat: animation, state: unfinished

--- en/index.php
+++ en/index.php
@@ -4,6 +4,8 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" 
         rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
    <meta charset="UTF-8"> 
+   <script src="../p5.js"></script>
+   
     <title>Assignement</title>
 </head> 
 
@@ -32,6 +34,8 @@
 ?>
 
 <body>
+  
+<script src="../sketch.js"></script>
 
   <br>  
   <section>
@@ -86,6 +90,12 @@
           </form>
       </div>
   </div> 
+</section>
+<br>
+<section>
+   <div class="container border" id="canvas_div"> <!--style="display:none;"> -->
+
+  </div>
 </section>
 <br>
 <section>
