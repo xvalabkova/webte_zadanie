e76b32c0a4f25d4826aa1a5b0ecd4d98b6a1b9a6
Frankiebyte <xhncin@stuba.sk>
2022-05-26 12:08:55 +0200

    add: script, style details

--- scripts/main.js
+++ scripts/main.js
@@ -5,16 +5,24 @@ document.getElementById("htmlData").value=document.getElementById("htmldata").ou
 document.getElementsByClassName("close")[0].onclick = function() {
 document.getElementById("modal").style.display = "none";
 
+
 }
+document.getElementsByClassName("close")[1].onclick = function() {
+ 
+  document.getElementById("modal2").style.display = "none";
+  
+  }
 window.onclick = function(event) {
 if (event.target == modal) {
 document.getElementById("modal").style.display = "none";
+document.getElementById("modal2").style.display = "none";
 }
   }
 
 document.getElementById("placeholder2").onclick = function() {
   document.getElementById("modal").style.display = "block";
   } 
+
 /*function show() { 
   console.log("show");
   document.getElementById("modal").style.display = "block";
